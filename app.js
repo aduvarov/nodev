d493345ba03e4321467ad6a16dd6a257fb72d8c6
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2022-12-13 23:53:07 +0600

    update: lesson-08

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const fs = require('fs');
+const http = require('http');
+const PORT = 3000;
 
-fs.readFile('./test.txt', 'utf-8', (error, data) => {
-    fs.mkdir('./files/', () => {
-        fs.writeFile('./files/test2.txt', `${data} \nНовый текст\n`, error => {
-            error ? console.log(error) : null;
-        });
-    });
+const server = http.createServer((req, res) => {
+    console.log('Server request');
+    console.log(req.method, req.url);
+
+    res.setHeader('Content-type', 'application/json');
+    // res.write('<head><link rel="stylesheet" href="#"></head>');
+    // res.write('<h1>Hello world!</h1>');
+    // res.write('<p>This is text content</p>');
+
+    const data = JSON.stringify([
+        { name: 'Anatoliy', age: 41 },
+        { name: 'Nastya', age: 39 },
+    ]);
+
+    res.end(data);
 });
 
-setTimeout(() => {
-    fs.unlink('./files/test2.txt', () => {});
-}, 4000);
-
-setTimeout(() => {
-    fs.rmdir('./files', () => {});
-}, 6000);
+server.listen(PORT, 'localhost', error => {
+    error ? console.error(error) : console.log(`Server is listening on port ${PORT}...`);
+});
