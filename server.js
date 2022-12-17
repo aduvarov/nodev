ff7fc06855ccd39cd655156ce53cdcbec2f17305
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2022-12-18 00:34:39 +0600

    update: lesson-13

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -12,6 +13,10 @@ const createPath = page => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
 app.listen(PORT, error => {
     error ? console.log(error) : console.log(`Server running on port ${PORT}`);
 });
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+app.use(express.static('styles'));
 
 app.get('/', (req, res) => {
     const title = 'Home';
