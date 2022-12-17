8a47a764f55047e037b4a3c7b44d1910147efff1
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2022-12-17 22:09:35 +0600

    update: lesson-12

--- server.js
+++ server.js
@@ -1,21 +1,46 @@
 const express = require('express');
 const path = require('path');
-const createPath = page => path.resolve(__dirname, 'views', `${page}.html`);
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 const PORT = 3000;
+
+const createPath = page => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
 
 app.listen(PORT, error => {
     error ? console.log(error) : console.log(`Server running on port ${PORT}`);
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(createPath('index'));
+    const title = 'Home';
+    res.render(createPath('index'), { title });
 });
 
 app.get('/contacts', (req, res) => {
-    res.sendFile(createPath('contacts'));
+    const title = 'Contacts';
+    const contacts = [
+        { name: 'YouTube', link: 'http://youtube.com/YauhenKavalchuk' },
+        { name: 'Twitter', link: 'http://twitter.com/YauhenKavalchuk' },
+        { name: 'GitHub', link: 'http://github.com/YauhenKavalchuk' },
+    ];
+    res.render(createPath('contacts'), { contacts, title });
+});
+
+app.get('/posts/:id', (req, res) => {
+    const title = 'Post';
+    res.render(createPath('post'), { title });
+});
+
+app.get('/posts', (req, res) => {
+    const title = 'Posts';
+    res.render(createPath('posts'), { title });
+});
+
+app.get('/add-post', (req, res) => {
+    const title = 'Add post';
+    res.render(createPath('add-post'), { title });
 });
 
 app.get('about-us', (req, res) => {
@@ -23,5 +48,6 @@ app.get('about-us', (req, res) => {
 });
 
 app.use((req, res) => {
-    res.status(404).sendFile(createPath('error'));
+    const title = 'Error page';
+    res.status(404).render(createPath('error'), { title });
 });
