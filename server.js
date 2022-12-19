c2d0303b44c0dd3886d32151e0f347e5cf649c60
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2022-12-20 00:49:02 +0600

    update: lesson-17

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Post = require('./models/post.js');
 const Contact = require('./models/contact');
+const config = require('./config.json');
+const methodOverride = require('method-override');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
 const PORT = 3000;
-const db = 'mongodb+srv://mongodmin:a-HgjB!3RTddStw@cluster0.dtv7cvd.mongodb.net/node-blog?retryWrites=true&w=majority';
+const db = config.db;
 mongoose.set('strictQuery', true);
 
 mongoose
@@ -29,6 +31,8 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static('styles'));
+
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     const title = 'Home';
@@ -49,6 +53,40 @@ app.get('/posts/:id', (req, res) => {
     const title = 'Post';
     Post.findById(req.params.id)
         .then(post => res.render(createPath('post'), { title, post }))
+        .catch(error => {
+            console.error(error);
+            res.render(createPath('error'), { title: 'Error' });
+        });
+});
+
+app.delete('/posts/:id', (req, res) => {
+    const title = 'Post';
+    Post.findByIdAndDelete(req.params.id)
+        .then(result => {
+            res.sendStatus(200);
+        })
+        .catch(error => {
+            console.error(error);
+            res.render(createPath('error'), { title: 'Error' });
+        });
+});
+
+app.get('/edit/:id', (req, res) => {
+    const title = 'Edit Post';
+    Post.findById(req.params.id)
+        .then(post => res.render(createPath('edit-post'), { title, post }))
+        .catch(error => {
+            console.error(error);
+            res.render(createPath('error'), { title: 'Error' });
+        });
+});
+
+app.put('/edit/:id', (req, res) => {
+    const { title, author, text } = req.body;
+    const { id } = req.params;
+
+    Post.findByIdAndUpdate(id, { title, author, text })
+        .then(result => res.redirect(`/posts/${id}`))
         .catch(error => {
             console.error(error);
             res.render(createPath('error'), { title: 'Error' });
